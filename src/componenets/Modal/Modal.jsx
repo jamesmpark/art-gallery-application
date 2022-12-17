0614a96f9b7a82f5c2995ec37189c53091f678cf
James <jamesmprk@gmail.com>
2022-12-17 14:47:56 -0800

    Resized images for different screen sizes and added background blur on modal

--- src/componenets/Modal/Modal.jsx
+++ src/componenets/Modal/Modal.jsx
@@ -1,13 +1,13 @@
-import Button from '../Button/Button';
 import { FaTimes } from 'react-icons/fa';
 import './Modal.css';
 
 const Modal = ({ open, onClose }) => {
+  // if not open return nothing
   if (!open) return null;
   return (
     <>
       <div className="modal">
-        <div className="overlay"></div>
+        <div className="overlay" onClick={onClose}></div>
         <div className="modal-content">
           <h2>Let's talk bzns</h2>
           <FaTimes onClick={onClose} />
